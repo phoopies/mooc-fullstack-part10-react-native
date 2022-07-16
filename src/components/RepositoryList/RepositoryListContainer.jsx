404662b368a2555ae4eeb87073612978a9cf777a
stefu <sotajagic@gmail.com>
2022-07-16 21:48:45 +0300

    part 10.23

--- src/components/RepositoryList/RepositoryListContainer.jsx
+++ src/components/RepositoryList/RepositoryListContainer.jsx
@@ -1,9 +1,32 @@
-import { FlatList, Pressable } from "react-native";
+import { FlatList, Pressable, StyleSheet } from "react-native";
+import { Picker } from '@react-native-picker/picker';
 import { useNavigate } from "react-router-native";
 import ItemSeparator from "../ItemSeperator";
 import RepositoryItem from "../RepositoryItem";
+import { useState } from "react";
 
-const RepositoryListContainer = ({ repositories }) => {
+const styles = StyleSheet.create({
+    picker: {
+        margin: 5,
+        padding: 5,
+    },
+})
+
+const OrderPicker = ({ setOrderBy, orderByOptions }) => {
+    const [selectedValue, setSelectedValue] = useState(orderByOptions[0]);
+
+    const onChange = (value) => { setSelectedValue(value); setOrderBy(value); }
+
+    return (<Picker
+        style={styles.picker}
+        selectedValue={selectedValue}
+        onValueChange={(itemValue) => onChange(itemValue)}
+    >
+        {orderByOptions.map(option => <Picker.Item key={option} label={option} value={option} />)}
+    </Picker>)
+}
+
+const RepositoryListContainer = ({ repositories, setOrderBy, orderByOptions }) => {
     const navigate = useNavigate();
     const onPress = (id) => {
         navigate(`/repository/${id}`);
@@ -17,9 +40,10 @@ const RepositoryListContainer = ({ repositories }) => {
     return (
         <FlatList
             data={repositoryNodes}
+            ListHeaderComponent={<OrderPicker setOrderBy={setOrderBy} orderByOptions={orderByOptions} />}
             ItemSeparatorComponent={ItemSeparator}
             keyExtractor={(item) => item.id}
-            renderItem={({item}) => <Pressable onPress={() => onPress(item.id)}>
+            renderItem={({ item }) => <Pressable onPress={() => onPress(item.id)}>
                 <RepositoryItem item={item} displayButton={false} />
             </Pressable>}
         />
--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import useRepositories from "../../hooks/useRepositories";
 import RepositoryListContainer from "./RepositoryListContainer";
 
-
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const options = {
+    "Latest": { orderBy: "CREATED_AT", orderDirection: "DESC" },
+    "Highest rated": { orderBy: "RATING_AVERAGE", orderDirection: "DESC" },
+    "Lowest rated": { orderBy: "RATING_AVERAGE", orderDirection: "ASC" },
+  }
+
+  const [orderBy, setOrderBy] = useState(Object.keys(options)[0]);
+  const { repositories } = useRepositories(options[orderBy]);
 
   return (
-    <RepositoryListContainer repositories={repositories} />
+    <RepositoryListContainer repositories={repositories} orderByOptions={Object.keys(options)} setOrderBy={setOrderBy} />
   );
 };
 
